1755bb2df33d9684375689f7f305fe297d7ccea2
Adriana Molina <moladriana@gmail.com>
2016-08-09 17:53:17 +0200

    Fixed bug: when a cell has already been used it can't be chosen again

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,103 +1,60 @@
 $(document).ready(function(){
   var xSign = false;
 
+  ////// FUNCTIONS //////
+
+  function fillCell (cell){
+    if($.trim($(cell).html())==""){
+      if (xSign){
+        $(cell).html("x");
+        xSign = false;
+      }
+      else{
+        $(cell).html("o");
+        xSign = true;
+      }
+    }
+    else{
+      console.log("ya está lleno");
+    }
+  }
+
+  ////// WHAT HAPPENS WHEN A CELL IS SELECTED //////
+
   $("#cell1").click(function(){
-    if (xSign){
-      $(this).html("x");
-      xSign = false;
-    }
-    else{
-      $(this).html("o");
-      xSign = true;
-    }
+    fillCell("#cell1");
   })
 
-    $("#cell2").click(function(){
-    if (xSign){
-      $(this).html("x");
-      xSign = false;
-    }
-    else{
-      $(this).html("o");
-      xSign = true;
-    }
+  $("#cell2").click(function(){
+    fillCell("#cell2");
   })
 
-    $("#cell3").click(function(){
-    if (xSign){
-      $(this).html("x");
-      xSign = false;
-    }
-    else{
-      $(this).html("o");
-      xSign = true;
-    }
+  $("#cell3").click(function(){
+    fillCell("#cell3");
   })
 
-    $("#cell4").click(function(){
-    if (xSign){
-      $(this).html("x");
-      xSign = false;
-    }
-    else{
-      $(this).html("o");
-      xSign = true;
-    }
+  $("#cell4").click(function(){
+    fillCell("#cell4");
   })
 
-    $("#cell5").click(function(){
-    if (xSign){
-      $(this).html("x");
-      xSign = false;
-    }
-    else{
-      $(this).html("o");
-      xSign = true;
-    }
+  $("#cell5").click(function(){
+    fillCell("#cell5");
   })
 
-    $("#cell6").click(function(){
-    if (xSign){
-      $(this).html("x");
-      xSign = false;
-    }
-    else{
-      $(this).html("o");
-      xSign = true;
-    }
+  $("#cell6").click(function(){
+    fillCell("#cell6");
   })
 
-    $("#cell7").click(function(){
-    if (xSign){
-      $(this).html("x");
-      xSign = false;
-    }
-    else{
-      $(this).html("o");
-      xSign = true;
-    }
+  $("#cell7").click(function(){
+    fillCell("#cell7");
   })
 
-    $("#cell8").click(function(){
-    if (xSign){
-      $(this).html("x");
-      xSign = false;
-    }
-    else{
-      $(this).html("o");
-      xSign = true;
-    }
+  $("#cell8").click(function(){
+    fillCell("#cell8");
   })
 
-    $("#cell9").click(function(){
-    if (xSign){
-      $(this).html("x");
-      xSign = false;
-    }
-    else{
-      $(this).html("o");
-      xSign = true;
-    }
+  $("#cell9").click(function(){
+    fillCell("#cell9");
   })
 
 
