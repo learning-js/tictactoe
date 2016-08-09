a88c0f0ca8f2153eadfaa76be086dc6310cc0526
Adriana Molina <moladriana@gmail.com>
2016-08-09 13:53:41 +0200

    Basic working game, alternate between X and O clicking on the cells

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,9 +1,103 @@
 $(document).ready(function(){
   var xSign = false;
-  var oSign = false;
 
   $("#cell1").click(function(){
-    $(this).html("x");
+    if (xSign){
+      $(this).html("x");
+      xSign = false;
+    }
+    else{
+      $(this).html("o");
+      xSign = true;
+    }
+  })
+
+    $("#cell2").click(function(){
+    if (xSign){
+      $(this).html("x");
+      xSign = false;
+    }
+    else{
+      $(this).html("o");
+      xSign = true;
+    }
+  })
+
+    $("#cell3").click(function(){
+    if (xSign){
+      $(this).html("x");
+      xSign = false;
+    }
+    else{
+      $(this).html("o");
+      xSign = true;
+    }
+  })
+
+    $("#cell4").click(function(){
+    if (xSign){
+      $(this).html("x");
+      xSign = false;
+    }
+    else{
+      $(this).html("o");
+      xSign = true;
+    }
+  })
+
+    $("#cell5").click(function(){
+    if (xSign){
+      $(this).html("x");
+      xSign = false;
+    }
+    else{
+      $(this).html("o");
+      xSign = true;
+    }
+  })
+
+    $("#cell6").click(function(){
+    if (xSign){
+      $(this).html("x");
+      xSign = false;
+    }
+    else{
+      $(this).html("o");
+      xSign = true;
+    }
+  })
+
+    $("#cell7").click(function(){
+    if (xSign){
+      $(this).html("x");
+      xSign = false;
+    }
+    else{
+      $(this).html("o");
+      xSign = true;
+    }
+  })
+
+    $("#cell8").click(function(){
+    if (xSign){
+      $(this).html("x");
+      xSign = false;
+    }
+    else{
+      $(this).html("o");
+      xSign = true;
+    }
+  })
+
+    $("#cell9").click(function(){
+    if (xSign){
+      $(this).html("x");
+      xSign = false;
+    }
+    else{
+      $(this).html("o");
+      xSign = true;
+    }
   })
 
 
