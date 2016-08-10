fe06a53e1d3f047d4d6fb0a23c22f87e4aacaa4a
Adriana Molina <moladriana@gmail.com>
2016-08-10 11:46:01 +0200

    Created array to save each player choice, and function that stores in the correct place of that array

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,16 +1,53 @@
 $(document).ready(function(){
   var xSign = false;
+  var gameSigns = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
 
   ////// FUNCTIONS //////
+
+  function fillArray (position, sign){
+    switch (position){
+      case "#cell1":
+        gameSigns[0][0] = sign;
+        break;
+      case "#cell2":
+        gameSigns[0][1] = sign;
+        break;
+      case "#cell3":
+        gameSigns[0][2] = sign;
+        break;
+      case "#cell4":
+        gameSigns[1][0] = sign;
+        break;
+      case "#cell5":
+        gameSigns[1][1] = sign;
+        break;
+      case "#cell6":
+        gameSigns[1][2] = sign;
+        break;
+      case "#cell7":
+        gameSigns[2][0] = sign;
+        break;
+      case "#cell8":
+        gameSigns[2][1] = sign;
+        break;
+      case "#cell9":
+        gameSigns[2][2] = sign;
+        break;
+    }
+  }
 
   function fillCell (cell){
     if($.trim($(cell).html())==""){
       if (xSign){
         $(cell).html("x");
+        fillArray(cell, "x")
+        console.log(gameSigns);
         xSign = false;
       }
       else{
         $(cell).html("o");
+        fillArray(cell, "o");
+        console.log(gameSigns);
         xSign = true;
       }
     }
